5718586a8d2faa312bdc90ed808d2e236112ca9f
Gabriel Glaser <gabriel.glaser@web.de>
2021-11-29 13:14:36 +0100

    update

--- src/main/java/standard/implementations/MyTextButton.java
+++ src/main/java/standard/implementations/MyTextButton.java
@@ -8,26 +8,29 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
-import javax.swing.border.BevelBorder;
+import javax.swing.SwingConstants;
 import standard.helper.CursorChangerOnHover;
 import standard.settings.Fonts;
 
 public class MyTextButton extends JLabel {
 
     private static final Color BACKGROUND = new Color(210, 210, 210);
-    private static final Color BACKGROUND_WHILE_HOVERED = new Color(190, 190, 190);
+    private static final Color BACKGROUND_WHILE_FOCUSED = new Color(190, 190, 190);
     private static final Color OF_TEXT = Color.BLACK;
 
     private final List<ActionListener> actionListeners = new ArrayList<>();
 
     public MyTextButton(final String text) {
 	super(text);
+	if (text.length() == 0) {
+	    throw new IllegalArgumentException("Button text has to contain atleast one character.");
+	}
 	setOpaque(true);
 	setBackground(BACKGROUND);
 	setForeground(OF_TEXT);
-	setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+	setHorizontalAlignment(SwingConstants.CENTER);
+	setVerticalAlignment(SwingConstants.CENTER);
 	setFont(Fonts.standard());
 	addMouseListener(new CursorChangerOnHover(new Cursor(Cursor.HAND_CURSOR)));
 	addMouseListener(new ButtonController());
@@ -38,6 +41,8 @@ public class MyTextButton extends JLabel {
     }
 
     private final class ButtonController implements MouseListener {
+
+	private Color old;
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -50,12 +55,13 @@ public class MyTextButton extends JLabel {
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-	    setBackground(BACKGROUND_WHILE_HOVERED);
+	    old = getBackground();
+	    setBackground(BACKGROUND_WHILE_FOCUSED);
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-	    setBackground(BACKGROUND);
+	    setBackground(old);
 	}
 
 	@Override
