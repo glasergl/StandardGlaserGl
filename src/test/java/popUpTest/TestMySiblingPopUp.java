fb0969aa4ab6bed2b4ad8aa8e4450c870e3bb2b2
Gabriel Glaser <gabriel.glaser@web.de>
2021-12-05 19:17:58 +0100

    update pop up listeners

--- src/test/java/popUpTest/TestMySiblingPopUp.java
+++ src/test/java/popUpTest/TestMySiblingPopUp.java
@@ -1,21 +1,27 @@
 package popUpTest;
 
+import java.awt.BorderLayout;
+
 import javax.swing.SwingUtilities;
 import standard.MyFrame;
-import standard.helper.listeners.PopUpDisplayerOnClick;
+import standard.helper.listeners.SiblingPopUpDisplayerOnClick;
+import standard.helper.listeners.SiblingPopUpDisplayerOnHover;
 import standard.implementations.MyLabel;
-import standard.implementations.MySiblingPopUp;
 
 public class TestMySiblingPopUp extends MyFrame {
 
-    MyLabel testLabel = new MyLabel("test");
-    MyLabel testPopUpComponent = new MyLabel("popUp hehe");
-    MySiblingPopUp testPopUp = new MySiblingPopUp(testPopUpComponent, this, testLabel);
+    MyLabel hoverTest = new MyLabel("hover");
+    MyLabel clickTest = new MyLabel("click");
+
+    MyLabel hoverPopUp = new MyLabel("hovered");
+    MyLabel clickPopUp = new MyLabel("clicked");
 
     public TestMySiblingPopUp() {
 	super();
-	add(testLabel);
-	testLabel.addMouseListener(new PopUpDisplayerOnClick(testPopUp));
+	add(hoverTest, BorderLayout.EAST);
+	add(clickTest, BorderLayout.WEST);
+	new SiblingPopUpDisplayerOnHover(hoverPopUp, hoverTest);
+	new SiblingPopUpDisplayerOnClick(clickPopUp, clickTest);
 	start();
     }
 
