4a5cf3473dbbe0e51e66cc6b50388570b2efcde8
Gabriel Glaser <gabriel.glaser@web.de>
2021-12-19 23:05:14 +0100

    format

--- src/test/java/implementations/TestTextField.java
+++ src/test/java/implementations/TestTextField.java
@@ -12,18 +12,17 @@ public class TestTextField {
 	SwingUtilities.invokeLater(() -> {
 	    MyTextField normalTextField = new MyTextField("With hint", "and initial text");
 	    MyNumberField numberField = new MyNumberField("NumberField", 20);
-
 	    MyTextField withNoInitialText = new MyTextField("No initial text, but with hint");
 	    MyTextField withNoHint = new MyTextField();
 	    MyTextField withoutAnything = new MyTextField();
 
 	    normalTextField.setColumns(20);
 	    numberField.setColumns(10);
-
 	    withNoInitialText.setColumns(30);
 	    withNoHint.setColumns(30);
 	    withNoHint.setText("With initial text, but no hint");
 	    withoutAnything.setColumns(15);
+	    
 	    TestFrame.showFrameWithComponents(normalTextField, numberField, withNoInitialText, withNoHint, withoutAnything);
 	});
     }
