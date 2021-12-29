fb47a270a6bef2e42cf401549aa31a9a8c6dc2cf
Gabriel Glaser <gabriel.glaser@web.de>
2021-12-29 20:18:30 +0100

    change Border

--- src/main/java/myComponent/textField/TextFieldAttributes.java
+++ src/main/java/myComponent/textField/TextFieldAttributes.java
@@ -3,12 +3,14 @@ package myComponent.textField;
 import java.awt.Color;
 import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
+
+import border.AnimatedUnderlineBorder;
 import settings.Colors;
 
 public class TextFieldAttributes {
 
     public static final Border UNFOCUSED_BORDER = new MatteBorder(0, 0, 2, 0, Colors.ofText());
-    public static final Border FOCUSED_BORDER = new MatteBorder(0, 0, 2, 0, Colors.ofFocus());
+    public static final Border FOCUSED_BORDER = new AnimatedUnderlineBorder(Colors.ofFocus(), Colors.ofText());
     public static final Color HINT_TEXT_COLOR = new Color(127, 127, 127);
     public static final float HINT_SIZE_FACTOR = 5.0f / 7.0f;
     public static final int NUMBER_OF_HINT_TRANSITION_STEPS = 15;
