fc1f8078cd3f48e0878977dc48f24c9dd0da4ba3
Gabriel Glaser <gabriel.glaser@web.de>
2021-12-30 15:41:08 +0100

    clean up and add doc

--- src/main/java/border/AnimatedUnderlineBorder.java
+++ src/main/java/border/AnimatedUnderlineBorder.java
@@ -13,64 +13,95 @@ import javax.swing.border.AbstractBorder;
  * underline is built up as an animation.
  *
  * @author Gabriel Glaser
- * @version 28.12.2021
+ * @version 30.12.2021
  */
-public class AnimatedUnderlineBorder extends AbstractBorder {
+public final class AnimatedUnderlineBorder extends AbstractBorder {
 
     private static final int STANDARD_THICKNESS = 2;
+    private static final Color STANDARD_BACKGROUND = new Color(0, 0, 0, 0);
+
     private static final int STANDARD_FIRST_STEP = 1;
-    private static final int MAX_ANIMATION_STEP = 20;
+    private static final int ANIMATION_STEPS = 20;
     private static final int MS_BETWEEN_ANIMATION_STEP = 10;
 
-    private final Color foreground;
-    private final Color background;
+    private final Color colorOfUnderline;
+    private final Color backgroundDuringAnimation;
     private final int thickness;
 
     private int currentAnimationStep;
 
-    private AnimatedUnderlineBorder(final Color foreground, final Color background, final int thickness, final int currentStep) {
-	this.foreground = foreground;
-	this.background = background;
+    /**
+     * @param colorOfUnderline
+     * @param backgroundDuringAnimation
+     * @param thickness
+     * @param currentAnimationStep
+     */
+    private AnimatedUnderlineBorder(final Color colorOfUnderline, final Color backgroundDuringAnimation, final int thickness, final int currentAnimationStep) {
+	assert 1 <= currentAnimationStep && currentAnimationStep <= ANIMATION_STEPS;
+
+	this.colorOfUnderline = colorOfUnderline;
+	this.backgroundDuringAnimation = backgroundDuringAnimation;
 	this.thickness = thickness;
-	this.currentAnimationStep = currentStep;
+	this.currentAnimationStep = currentAnimationStep;
     }
 
+    /**
+     * @param foreground - Color of the underline.
+     * @param background - Background which is visible while the underline is built.
+     * @param thickness  - Thickness of the resulting underline.
+     */
     public AnimatedUnderlineBorder(final Color foreground, final Color background, final int thickness) {
 	this(foreground, background, thickness, STANDARD_FIRST_STEP);
     }
 
+    /**
+     * Constructor with default thickness of 2.
+     * 
+     * @param foreground - Color of the underline.
+     * @param background - Background which is visible while the underline is built.
+     */
     public AnimatedUnderlineBorder(final Color foreground, final Color background) {
 	this(foreground, background, STANDARD_THICKNESS);
     }
 
-    public AnimatedUnderlineBorder(final Color color) {
-	this(color, new Color(0, 0, 0, 0));
+    /**
+     * Constructor with default invisible background and thickness of 2.
+     * 
+     * @param underlineColor
+     */
+    public AnimatedUnderlineBorder(final Color underlineColor) {
+	this(underlineColor, STANDARD_BACKGROUND);
     }
 
     @Override
-    public void paintBorder(Component component, Graphics context, int x, int y, int width, int height) {
-	context.setColor(background);
+    public void paintBorder(final Component component, final Graphics context, final int x, final int y, final int width, final int height) {
+	context.setColor(backgroundDuringAnimation);
 	context.fillRect(x, height - thickness, width, thickness);
-	context.setColor(foreground);
-	final int currentWidth = (int) (((double) currentAnimationStep / MAX_ANIMATION_STEP) * width);
+	context.setColor(colorOfUnderline);
+	final int currentWidth = (int) (((double) currentAnimationStep / ANIMATION_STEPS) * width);
 	final int middleX = (width / 2) - (currentWidth / 2);
 	context.fillRect(middleX, height - thickness, currentWidth, thickness);
-	if (currentAnimationStep <= MAX_ANIMATION_STEP) {
-	    final Timer timer = new Timer(MS_BETWEEN_ANIMATION_STEP, (repaint) -> {
-		((JComponent) component).setBorder(new AnimatedUnderlineBorder(foreground, background, thickness, currentAnimationStep + 1));
-	    });
-	    timer.setRepeats(false);
-	    timer.start();
+	if (currentAnimationStep <= ANIMATION_STEPS) {
+	    setupNextStep(component);
 	}
     }
 
-    @Override
-    public Insets getBorderInsets(Component c) {
-	return getBorderInsets(c, new Insets(0, 0, 0, 0));
+    private void setupNextStep(final Component component) {
+	final Timer timer = new Timer(MS_BETWEEN_ANIMATION_STEP, (nextStep) -> {
+	    final JComponent jComponent = (JComponent) component;
+	    jComponent.setBorder(new AnimatedUnderlineBorder(colorOfUnderline, backgroundDuringAnimation, thickness, currentAnimationStep + 1));
+	});
+	timer.setRepeats(false);
+	timer.start();
     }
 
     @Override
-    public Insets getBorderInsets(Component c, Insets insets) {
+    public Insets getBorderInsets(final Component component) {
+	return getBorderInsets(component, new Insets(0, 0, 0, 0));
+    }
+
+    @Override
+    public Insets getBorderInsets(final Component component, Insets insets) {
 	insets.set(0, 0, thickness, 0);
 	return insets;
     }
--- src/test/java/border/test/TestAnimatedUnderlineBorder.java
+++ src/test/java/border/test/TestAnimatedUnderlineBorder.java
@@ -1,9 +1,8 @@
 package border.test;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.MouseEvent;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import border.AnimatedUnderlineBorder;
@@ -14,18 +13,17 @@ public class TestAnimatedUnderlineBorder {
 
     public static void main(String[] args) {
 	SwingUtilities.invokeLater(() -> {
-	    JPanel panel = new JPanel();
-	    panel.setPreferredSize(new Dimension(100, 100));
-	    panel.addMouseListener(new MyMouseListener() {
+	    JLabel label = new JLabel("Click me to see Border");
+	    label.addMouseListener(new MyMouseListener() {
 		boolean drawAnimatedBorder = true;
 
 		@Override
 		public void mouseClicked(final MouseEvent click) {
-		    panel.setBorder(drawAnimatedBorder ? new AnimatedUnderlineBorder(Color.RED, Color.GREEN, 5) : new EmptyBorder(0, 0, 0, 0));
+		    label.setBorder(drawAnimatedBorder ? new AnimatedUnderlineBorder(Color.GRAY, Color.RED, 5) : new EmptyBorder(0, 0, 0, 0));
 		    drawAnimatedBorder = !drawAnimatedBorder;
 		}
 	    });
-	    TestFrame.showFrameWithComponents(panel);
+	    TestFrame.showFrameWithComponents(label);
 	});
     }
 
