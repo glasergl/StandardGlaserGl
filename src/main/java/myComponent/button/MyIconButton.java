575cf82865daed8201acedb94b63f0540ded5ff1
Gabriel Glaser <gabriel.glaser@web.de>
2021-12-30 22:42:23 +0100

    finish first version of implementation

--- src/main/java/myComponent/button/MyIconButton.java
+++ src/main/java/myComponent/button/MyIconButton.java
@@ -1,47 +1,57 @@
 package myComponent.button;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
+import entity.ColorType;
+import eventListener.ColorChangerOnHover;
 import eventListener.CursorChangerOnHover;
 import eventListener.emptyImplementation.MyMouseListener;
+import general.SwingFunctions;
 import settings.Colors;
 
-public class MyIconButton extends JPanel {
+public class MyIconButton extends JLabel {
 
-    private final JLabel label = new JLabel();
+    private final List<ActionListener> actionListeners = new ArrayList<>();
 
     public MyIconButton(final ImageIcon toDisplay, final int width, final int height) {
-	super(new BorderLayout());
-	final ImageIcon scaled = new ImageIcon(toDisplay.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
-	label.setIcon(scaled);
-	label.setBorder(new EmptyBorder(10, 10, 10, 10));
-	add(label);
-	addMouseListener(new ButtonController());
-	addMouseListener(new CursorChangerOnHover(new Cursor(Cursor.HAND_CURSOR)));
+	super(SwingFunctions.scale(toDisplay, width, height));
+	setup();
     }
 
-    private class ButtonController implements MyMouseListener {
+    public MyIconButton(final ImageIcon toDisplay) {
+	this(toDisplay, toDisplay.getIconWidth(), toDisplay.getIconHeight());
+    }
 
-	private Optional<Color> oldBackground = Optional.empty();
+    public void addActionListener(final ActionListener toAdd) {
+	actionListeners.add(toAdd);
+    }
 
+    public void removeActionListener(final ActionListener toRemove) {
+	actionListeners.remove(toRemove);
+    }
+
+    private void setup() {
+	setOpaque(true);
+	setFocusable(true);
+	addMouseListener(new ColorChangerOnHover(Colors.ofFocus(), ColorType.BACKGROUND));
+	addMouseListener(new CursorChangerOnHover(new Cursor(Cursor.HAND_CURSOR)));
+	addMouseListener(new ActionActivater());
+    }
+
+    private final class ActionActivater implements MyMouseListener {
 	@Override
-	public void mouseEntered(final MouseEvent e) {
-	    oldBackground = Optional.of(getBackground());
-	    setBackground(Colors.ofFocus());
-	}
-
-	@Override
-	public void mouseExited(final MouseEvent e) {
-	    setBackground(oldBackground.get());
+	public void mouseClicked(MouseEvent mouseClickEvent) {
+	    final ActionEvent click = new ActionEvent(MyIconButton.this, ActionEvent.ACTION_PERFORMED, "");
+	    for (final ActionListener actionListener : actionListeners) {
+		actionListener.actionPerformed(click);
+	    }
+	    requestFocus();
 	}
     }
 
