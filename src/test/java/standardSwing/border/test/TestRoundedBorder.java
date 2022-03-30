79278bb241efc27bdfc0cf74b971d8fbbb6e2f62
Gabriel Glaser <gabriel.glaser@web.de>
2022-03-30 16:03:32 +0200

    add round border

---
+++ src/test/java/standardSwing/border/test/TestRoundedBorder.java
@@ -0,0 +1,25 @@
+package standardSwing.border.test;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import standardSwing.border.LeftRightRoundBorder;
+import standardSwing.container.test.TestFrame;
+
+/**
+ * @author Gabriel Glaser
+ * @version 30.03.2022
+ */
+public class TestRoundedBorder {
+
+    public static void main(String[] args) {
+	SwingUtilities.invokeLater(() -> {
+	    JLabel label = new JLabel("round border");
+	    label.setOpaque(true);
+	    label.setBackground(Color.CYAN);
+	    label.setBorder(new LeftRightRoundBorder());
+	    TestFrame.showFrameWithComponents(label);
+	});
+    }
+
+}
