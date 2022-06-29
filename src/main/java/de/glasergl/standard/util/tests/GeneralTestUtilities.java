a9191da02e1fd27e3296176f04782c17c8b65671
Gabriel Glaser <gabriel.glaser@web.de>
2022-06-30 01:54:23 +0200

    add general test utilities with test base path

---
+++ src/main/java/de/glasergl/standard/util/tests/GeneralTestUtilities.java
@@ -0,0 +1,14 @@
+package de.glasergl.standard.util.tests;
+
+/**
+ * Class which contains variables and functions which are generally applicable
+ * for tests with JUnit.
+ *
+ * @author Gabriel Glaser
+ * @version 30.06.2022
+ */
+public final class GeneralTestUtilities {
+
+    public static final String BASE_TEST_PATH = "src\\test\\resources\\";
+
+}
